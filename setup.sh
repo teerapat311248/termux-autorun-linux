bf12ca2c550dc5dce80e11b7e1c7c7ab0f3f6492
Phumin Maliwan <32133224+mantvmass@users.noreply.github.com>
2021-10-14 06:29:11 +0700

    fig error

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 cd /data/data/com.termux/files/usr/etc
 rm profile
-cd
-cd open-auto-file-open
-mv profile ../../usr/etc
+cd /data/data/com.termux/files/home/open-auto-file-open
+mv profile ../../usr/etc/
 cd
 rm -rf open-auto-file-open
