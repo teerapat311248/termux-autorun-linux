5af005b89a586c5974e5f079e100bab38a0c97e1
Phumin Maliwan <32133224+mantvmass@users.noreply.github.com>
2021-10-14 17:15:46 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 cd /data/data/com.termux/files/usr/etc
 rm profile
-cd /data/data/com.termux/files/home/open-auto-file-one
+cd /data/data/com.termux/files/home/auto-file-one
 mv profile ../../usr/etc/
 mv termux ../
 cd ..
